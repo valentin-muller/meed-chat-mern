af5a39287ad5033b46ed5af101bd8c587061284b
valentin-muller <58423332+valentin-muller@users.noreply.github.com>
2020-09-09 11:53:43 +0200

    CSS DONE

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,8 @@ import { Avatar, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Chat.css";
 import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
@@ -25,6 +27,40 @@ function Chat() {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Valentin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Valentin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Valentin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            placeholder="Type a message"
+            type="text"
+          />
+          <button type="submit">
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
